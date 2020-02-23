395d01987db45c06dcc61ea08f28e00a43c64a02
Snx <59105899+Snnxx@users.noreply.github.com>
2020-02-23 23:15:38 +0100

    Update

---
+++ src/main/java/de/snx/spexmc/Adds.java
@@ -0,0 +1,4 @@
+package de.snx.spexmc;
+
+public class Adds {
+}
---
+++ src/main/java/de/snx/spexmc/listener/PlayerJoinListener.java
@@ -0,0 +1,12 @@
+package de.snx.spexmc.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class PlayerJoinListener implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(Player){
+
+    }
+}
